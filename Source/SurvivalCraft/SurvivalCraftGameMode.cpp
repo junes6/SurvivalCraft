b00e09b0c50d2e0b9d2404d7252024caa064717e
jangdave <121008922+atg99@users.noreply.github.com>
2023-03-24 22:55:03 +0900

    1

--- Source/SurvivalCraft/SurvivalCraftGameMode.cpp
+++ Source/SurvivalCraft/SurvivalCraftGameMode.cpp
@@ -8,7 +8,8 @@ ASurvivalCraftGameMode::ASurvivalCraftGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPerson/Blueprints/BP_FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/_Main/Blueprints/BP_FirstPersonCharacter"));
+	
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 }
